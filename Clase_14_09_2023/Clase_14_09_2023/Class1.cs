65a2a90767be391dfd1df5e4a0eba7fc7f3760d3
JoanRamos555 <145037603+JoanRamos555@users.noreply.github.com>
2023-09-14 10:06:06 -0500

    EJERCICIO_CALCULADORA

---
+++ Clase_14_09_2023/Clase_14_09_2023/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_14_09_2023
+{
+    internal class Class1
+    {
+        public int Suma1(int a,int b)
+        {
+            Console.WriteLine("El resultado de la suma es: ");
+            return a + b;
+        }
+        public int multiplicar1(int a, int b)
+        {
+            Console.WriteLine("El resultado de la multiplicacion es: ");
+            return a*b;
+        }
+        public void dividir1(int a, int b)
+        {
+
+
+            if (b == 0)
+            {
+                do
+                {
+                    Console.WriteLine("Error, no se puede dividir por 0");
+
+                    Console.WriteLine("Digite otro numero para el denominador");
+                    b = int.Parse(Console.ReadLine());
+
+
+                } while (b == 0);
+
+            }
+
+            Console.WriteLine("El resultado de la division es: ");
+            Console.WriteLine(a / b);
+
+        }
+
+
+    }
+}
---
+++ Clase_14_09_2023/Clase_14_09_2023/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_14_09_2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool t = false;
+            do
+            {
+                t = false;
+                String b;
+                int x;
+                int y;
+
+                Console.WriteLine("Digite un numero");
+                x = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite un numero");
+                y = int.Parse(Console.ReadLine());
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Suma");
+
+                Class1 opera = new Class1();
+                int total1 = opera.Suma1(x, y);
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine(total1);
+    
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Multiplicacion");
+                int total2 = opera.multiplicar1(x, y);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(total2);
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Division");
+
+                Class1 opera3 = new Class1();
+                opera.dividir1(x, y);
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                Console.WriteLine("Desea volver a operar?");
+
+                b = Console.ReadLine();
+
+
+                if (b=="no")
+                {
+                    t = true;
+                }
+                else
+                {
+                    t = false;
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+
+            } while (t==false);
+
+        }
+    }
+}
